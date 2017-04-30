a16df2970666a9ccc185e8e4485c3d1db6f67d4c
anniepa <anniepa246@gmail.com>
2017-04-30 17:13:27 -0400

    full pipes
    pipeline implementation

--- iplc-sim.c
+++ iplc-sim.c
@@ -313,9 +313,6 @@ void iplc_sim_dump_pipeline()
  */
 void iplc_sim_push_pipeline_stage()
 {
-    int i;
-    int data_hit=1;
-    
     /* 1. Count WRITEBACK stage is "retired" -- This I'm giving you */
     if (pipeline[WRITEBACK].instruction_address) {
         instruction_count++;
@@ -323,25 +320,84 @@ void iplc_sim_push_pipeline_stage()
             printf("DEBUG: Retired Instruction at 0x%x, Type %d, at Time %u \n",
                    pipeline[WRITEBACK].instruction_address, pipeline[WRITEBACK].itype, pipeline_cycles);
     }
-    
+    //now to count stalls and misses
+    int nopes = 0;
+    int misses = 0;
+
     /* 2. Check for BRANCH and correct/incorrect Branch Prediction */
     if (pipeline[DECODE].itype == BRANCH) {
-        int branch_taken = 0;
+        int branch_taken = 1;
+        branch_count++;
+
+        if(pipeline[FETCH].instruction_address ==
+            (pipeline[DECODE].instruction_address + 4)){
+            branch_taken = 0;
+        }
+        if(branch_predict_taken == branch_taken){
+            correct_branch_predictions++;
+        }else{
+            nopes++;
+        }
     }
     
     /* 3. Check for LW delays due to use in ALU stage and if data hit/miss
      *    add delay cycles if needed.
      */
     if (pipeline[MEM].itype == LW) {
-        int inserted_nop = 0;
+        //must check for stalls if lw is loading branch or rtype
+        //rtype:
+        if (pipeline[ALU].itype == RTYPE &&
+            (pipeline[MEM].stage.lw.dest_reg ==
+             pipeline[ALU].stage.rtype.reg1 ||
+            pipeline[MEM].stage.lw.dest_reg == 
+            pipeline[ALU].stage.rtype.reg2_or_constant)) {
+            nopes++;
+        }else if (pipeline[ALU].itype == BRANCH &&
+            (pipeline[MEM].stage.lw.dest_reg == 
+                pipeline[ALU].stage.branch.reg1 ||
+            pipeline[MEM].stage.lw.dest_reg == 
+            pipeline[ALU].stage.branch.reg2)){
+            nopes++;
+        }
+
+        //now check for hits or misses
+        if(iplc_sim_trap_address(pipeline[MEM].stage.lw.data_address)){
+            //this is a hit
+            printf("DATA HIT:\t Address 0x%x \n", pipeline[MEM].stage.lw.data_address);
+        }else{
+            //miss
+            printf("DATA MISS:\t Address 0x%x \n", pipeline[MEM].stage.lw.data_address);
+            //miss penalty
+            pipeline_cycles += 10;
+            misses++;
+        }
     }
     
     /* 4. Check for SW mem acess and data miss .. add delay cycles if needed */
     if (pipeline[MEM].itype == SW) {
+        //another hit miss check
+        if(iplc_sim_trap_address(pipeline[MEM].stage.lw.data_address)){
+            //hit
+            printf("DATA HIT:\t Address 0x%x \n", pipeline[MEM].stage.lw.data_address);
+        }else{
+            //miss
+            printf("DATA MISS:\t Address 0x%x \n", pipeline[MEM].stage.lw.data_address);
+            //penalties
+            pipeline_cycles += 10;
+            misses++;
+        }
     }
     
     /* 5. Increment pipe_cycles 1 cycle for normal processing */
+    if(misses == 0){
+        pipeline_cycles += (nopes + 1);
+    }
     /* 6. push stages thru MEM->WB, ALU->MEM, DECODE->ALU, FETCH->ALU */
+    for (int i = MAX_STAGES - 1; i > 0; i--) {
+        pipeline[i].itype = pipeline[i-1].itype;
+        pipeline[i].instruction_address = pipeline[i-1].instruction_address;
+        pipeline[i].stage = pipeline[i-1].stage;
+    }
     
     // 7. This is a give'me -- Reset the FETCH stage to NOP via bezero */
     bzero(&(pipeline[FETCH]), sizeof(pipeline_t));
